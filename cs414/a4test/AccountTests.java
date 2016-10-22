abf43196bdc59b21903f7d48beca7bcad894d03a
Huijun Wang <jevelyn@rams.colostate.edu>
2016-10-22 16:36:18 -0600

    add 2 tests

---
+++ cs414/a4test/AccountTests.java
@@ -0,0 +1,14 @@
+package cs414.a4test;
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class AccountTests {
+
+	@Test
+	public void test() {
+
+	}
+
+}
