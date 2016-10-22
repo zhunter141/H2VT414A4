bf9c4f0b431c8fbf2b0657a9bd476d8bb091ef0b
tzujuiyu <TJ@tzujuiyus-MacBook-Air.local>
2016-10-22 15:23:14 -0600

    changed hashSet type for myDeeds

--- cs414/a4/Controller.java
+++ cs414/a4/Controller.java
@@ -1,9 +1,15 @@
 package cs414.a4;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.HashSet;
+
+import javax.swing.JButton;
+import javax.swing.text.View;
 
 public class Controller {
 	private Model model;
-	
+	private Deed d;
 	public Controller()
 	  {}
 	 
@@ -12,7 +18,48 @@ public class Controller {
 	 
 	public void addView(View v)
 	  {view = v;}
-	 
-	 
+	
+	//Build/Return a Buy button: it calls the buyDeedThroughButton method in model
+	JButton getBuyButton(){
+		  JButton a = new JButton();
+		  
+		  a.addActionListener(new ActionListener() {
+			  public void actionPerformed(ActionEvent e)
+			  {
+				  System.out.println("Debug-Controller: " + "Buy button pressed");
+				  model.buyDeedThroughButton();} 
+		  });
+		  
+		  return a;
+	}
+	
+	JButton getSellButton(){
+		  HashSet<Square> temp = model.getDeeds();//deeds = property
+		  //Hand view class deeds for player to select which deed to sell
+		  View.chooseDeed(temp);
+		  JButton b = new JButton();
+		  
+		  b.addActionListener(new ActionListener() {
+			  public void actionPerformed(ActionEvent e)
+			  {
+				  System.out.println("Debug-Controller: " + "Sell button pressed");
+				  model.sellDeedThroughButton(temp);} 
+		  });
+		  
+		  return b;
+	}
+	
+	JButton getRollDiceButton(){
+		  JButton c = new JButton();
+		  
+		  c.addActionListener(new ActionListener() {
+			  public void actionPerformed(ActionEvent e)
+			  {
+				  System.out.println("Debug-Controller: " + "Roll Dice button pressed");
+				  model.rollDiceThroughButton();} 
+		  });
+		  
+		  return c;
+	}
 	 
 }
--- cs414/a4/Player.java
+++ cs414/a4/Player.java
@@ -6,7 +6,7 @@ import java.util.Set;
 public class Player {
 	private int id;
 	private String name;
-	private HashSet myDeeds;
+	private HashSet<Square> myDeeds;
 	private Token token;
 	
 	public Player(int id, String name, Token token) {
@@ -27,7 +27,7 @@ public class Player {
 		return token;
 	}
 	
-	public HashSet getMyDeeds(){
+	public HashSet<Square> getMyDeeds(){
 		return myDeeds;
 	}
 	
