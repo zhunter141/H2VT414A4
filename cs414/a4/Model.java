8c61a7de84ca00f6735210d9544b324645239e79
Huijun Wang <jevelyn@rams.colostate.edu>
2016-10-22 16:29:23 -0600

    change Model

--- cs414/a4/Model.java
+++ cs414/a4/Model.java
@@ -9,7 +9,7 @@ public class Model {
 	
 	private Player[] allPlayers = new Player[4];
 	private HashSet<Token> allTokens;
-	private HashSet<Deed> allDeeds;
+
 	private Board board;
 	private Bank monopolyBank;
 	private Dice dice;
@@ -37,44 +37,69 @@ public class Model {
 		return curPlayer;
 	}
 	
-	void rollDiceThroughButton(Dice d){
-		int steps = d.roll();
+	void rollDiceThroughButton(){
+		int steps = dice.roll();
 		curPlayer = allPlayers[iterator%counter];
 
 		board.move(steps,curPlayer.getToken());
 
 		
 		//Refactor later maybe
-		
-		Square newSqr = curPlayer.getToken().getLoc();
-		//Is a deed
-		if(newSqr instanceof Deed){
 		//Do nothing because the player can click the button"Buy a deed"
-		}
-		//Utility or a RailRoad
-		else{
-			if(newSqr instanceof Utility){
-				Utility utility =  (Utility)newSqr;
-				int cost = utility.getCost();
-				monopolyBank.payDue(curPlayer, cost);
-				monopolyBank.withdrawl(utility.getOwner(), cost);
-				
-			}
-			else{
-				RailRoad railRoad =  (RailRoad)newSqr;
-				int cost = railRoad.getCost();
 
+		Square newSqr = curPlayer.getToken().getLoc();
+
+
+		if(newSqr instanceof Utility){
+			    Utility utility = (Utility)newSqr;
+			
+			
+			    if(newSqr.getOwner() == null){
+				//Do nothing because the player can click the button"Buy a deed"	
+				}
+				//pay rent
+				else{
+					int cost = utility.getRentCost();
+					monopolyBank.payDue(curPlayer, cost);
+					monopolyBank.withdrawl(utility.getOwner(), cost);
+				}
+
+			
+		}
+		else if(newSqr instanceof Deed){
+				Deed deed = (Deed)newSqr;
+				
+				
+			    if(deed.getOwner() == null){
+				//Do nothing because the player can click the button"Buy a deed"	
+				}
+				//pay rent
+				else{
+					int cost = deed.getRentCost();
+					monopolyBank.payDue(curPlayer, cost);
+					monopolyBank.withdrawl(deed.getOwner(), cost);
+				}
+
+		}
+		else if(newSqr instanceof RailRoad){
+			RailRoad railRoad = (RailRoad)newSqr;
+			
+			
+		    if(railRoad.getOwner() == null){
+			//Do nothing because the player can click the button"Buy a deed"	
+			}
+			//pay rent
+			else{
+				int cost = railRoad.getRentCost();
 				monopolyBank.payDue(curPlayer, cost);
 				monopolyBank.withdrawl(railRoad.getOwner(), cost);
-
-
 			}
 
+
 		}
-		
-		
-		
-		
+		else{
+			
+		}
 		
 		
 		iterator++;
@@ -103,39 +128,114 @@ public class Model {
 		
 	}
 	
+	// In this method, deed is a utility, railroad, deed
 	//Pay attention on choose deed
-	void sellDeedThroughButton(Deed d){
+	void sellDeedThroughButton(Square d){
 		//removeDeeds()
 		curPlayer.removeDeeds(d);
-		monopolyBank.withdrawl(curPlayer, d.getCost());
 		
 		
+		if(d instanceof Utility){
+			Utility utility =  (Utility)d;
+			d =  (Utility)d;
+
+			int cost = utility.getCost();
+			
+			monopolyBank.payDue(curPlayer, cost);
+			monopolyBank.withdrawl(utility.getOwner(), cost);
+			
+			
+		}
+		else if(d instanceof Deed){
+			Deed deed =  (Deed)d;
+			d =  (Deed)d;
+
+			int cost = deed.getCost();
+			
+			monopolyBank.payDue(curPlayer, cost);
+			monopolyBank.withdrawl(deed.getOwner(), cost);
+			
+		}
+		else if(d instanceof RailRoad){
+			RailRoad railRoad =  (RailRoad)d;
+			d =  (RailRoad)d;
+
+			int cost = railRoad.getCost();
+
+			monopolyBank.payDue(curPlayer, cost);
+			monopolyBank.withdrawl(railRoad.getOwner(), cost);
+
+
+		}
+		else{}
+		
+
+		//may go wrong because of the type
+		d.setOwner(null);
 		
 		
 		if (view != null)    {
 		      //view.update();
 		}
 	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 	void buyDeedThroughButton(){
-		
-		
-		
-		
-		
+		Square cursqr = curPlayer.getToken().getLoc();
+		Square sqrCopy = cursqr;
+		int cost = 0;
+
+		if(cursqr.isPurchasable() != true){}
+		else{
+			if(cursqr instanceof Utility){
+				Utility utility =  (Utility)cursqr;
+				cursqr =  (Utility)cursqr;
+	
+				cost = utility.getCost();
+				
+			
+				
+			}
+			else if(cursqr instanceof Deed){
+				Deed deed =  (Deed)cursqr;
+				cursqr =  (Deed)cursqr;
+	
+				cost = deed.getCost();
+				
+			}
+			else if(cursqr instanceof RailRoad){
+				RailRoad railRoad =  (RailRoad)cursqr;
+				cursqr =  (RailRoad)cursqr;
+	
+				cost = railRoad.getCost();
+	
+	
+	
+			}
+			else{}
+			
+			monopolyBank.payDue(curPlayer, cost);
+			monopolyBank.withdrawl(cursqr.getOwner(), cost);
+			
+			curPlayer.addDeed(sqrCopy);
+			cursqr.setOwner(curPlayer);
+}
 		if (view != null)    {
 		      //view.update();
 		}
 	}
 	
-	void startThroughButton(){
-		
-		
-		
-		
-		
-		
-	}
+	
+	
+	
 	
 	
 	//get status aka give status to view/others
@@ -147,8 +247,9 @@ public class Model {
 	 public HashSet<Token> getTokens(){
 		 return allTokens;
 	 }
-	 public HashSet<Deed> getDeeds(){
-		 return allDeeds;
+	 
+	 public HashSet<Square> getDeeds(){
+		 return curPlayer.getMyDeeds();
 	 }
 	 
 	 public Board getBoard(){
