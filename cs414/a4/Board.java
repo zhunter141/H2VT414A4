b76069c29b4a731e0f2d0ad3136819100d343130
Victor Fuentes Sangabriel <vfuentes1213@gmail.com>
2016-10-22 13:29:21 -0600

    Changes in constructor for Model.

--- cs414/a4/Board.java
+++ cs414/a4/Board.java
@@ -4,6 +4,8 @@
 package cs414.a4;
 
 public class Board {
+	private Square start;
+	
 	public Square initialize(){
 		/*
 		 *  Board layout is based on traditional Monopoly Game US version
@@ -14,19 +16,36 @@ public class Board {
 		 */
 		  
 		
-		Square start = new Square("GREY","GO");
+		start = new Square("GREY","GO");
 		
 		// First 10 squares creation
-		Deed   ma = new Deed("PURPLE","MIDITERRANEAN AVENUE",60,50,50);
-		Square cc = new Square("GREY","COMMUNITY CHEST");
-		Deed   ba = new Deed("PURPLE","BALTIC AVENUE",60,50,50);
-		Square it = new Square("GREY","INCOME TAX");
+		Deed   kr = new Deed("BROWN","OLD KENT ROAD",60,50,50);
+		Square cc = new Square("GRAY","COMMUNITY CHEST");
+		Deed   wr = new Deed("BROWN","WHITECHAPEL ROAD",60,50,50);
+		Square it = new Square("GRAY","INCOME TAX");
+		RailRoad kc = new RailRoad("GRAY","KINGS CROSS STATION",200);
+		Deed ai = new Deed("BLUE","THE ANGEL ISLINGTON",100,50,50);
+		Square ch1 = new Square("GRAY","CHANCE");
+		Deed er    = new Deed("BLUE","EUSTON ROAD",100,50,50);
 		
 		// Connection of first 10 squares
-		start.setNext(ma);
-		ma.setNext(cc);
-		cc.setNext(ba);
-		ba.setNext(it);
+		start.setNext(kr);
+		kr.setNext(cc);
+		cc.setNext(wr);
+		wr.setNext(it);
+		it.setNext(kc);
+		kc.setNext(ai);
+		ai.setNext(ch1);
+		ch1.setNext(er);
+		
+		// Second 10 Squares creation
+		Deed ma = new Deed("PURPLE","MIDITERRANEAN AVENUE",60,50,50);
+		Deed ba = new Deed("PURPLE","BALTIC AVENUE",60,50,50);
+		
+		// Connection of second 10 squares
+		er.setNext(ma);
+		ma.setNext(ba);
+		ba.setNext(start);
 		
 		return start;
 	}
@@ -35,5 +54,9 @@ public class Board {
 		for(int i=0;i<steps;i++){
 			t.move();
 		}
+	}
+	
+	public Square getStart(){
+		return start;
 	}
 }
