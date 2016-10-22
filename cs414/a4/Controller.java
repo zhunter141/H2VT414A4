823262798a438d337bd7e7a5503d7a7d82644c0f
tzujuiyu <TJ@tzujuiyus-MacBook-Air.local>
2016-10-22 12:35:50 -0600

    added addDeed() to Player.java

--- cs414/a4/Controller.java
+++ cs414/a4/Controller.java
@@ -1,5 +1,18 @@
 package cs414.a4;
 
-public class Controller {
 
+public class Controller {
+	private Model model;
+	
+	public Controller()
+	  {}
+	 
+	public void addModel(Model m)
+	  {model = m;}
+	 
+	public void addView(View v)
+	  {view = v;}
+	 
+	 
+	 
 }
