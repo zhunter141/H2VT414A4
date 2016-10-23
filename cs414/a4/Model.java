188c5983d8b38010eb919d8d4e001d55132b983a
Huijun Wang <jevelyn@rams.colostate.edu>
2016-10-23 12:45:00 -0600

    add msg in rollDice

--- cs414/a4/Model.java
+++ cs414/a4/Model.java
@@ -55,6 +55,7 @@ public class Model {
 	public void startGame(){
 		// Start the game by setting the current player
 		currPlayer = players[0];
+		msg = currPlayer.getName();
 	}
 	public void rollDiceThroughButton(){
 		int steps = dice.roll();
