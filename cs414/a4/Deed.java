609bbc33d9dafcf76740eb4da9b8eabb1fad6f2a
Huijun Wang <jevelyn@rams.colostate.edu>
2016-10-25 15:16:44 -0600

    nothing

--- cs414/a4/Deed.java
+++ cs414/a4/Deed.java
@@ -1,5 +1,7 @@
 package cs414.a4;
 
+import java.awt.Color;
+
 public class Deed extends Square {
 	private int cost;
 	private int houseCost;
@@ -12,7 +14,7 @@ public class Deed extends Square {
 
 
 	
-	public Deed(String color, String name,int cost,int houseCost,int hotelCost, int rentCost) {
+	public Deed(Color color, String name,int cost,int houseCost,int hotelCost, int rentCost) {
 		super(color,name);
 		this.cost = cost;
 		this.houseCost = houseCost;
